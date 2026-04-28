1afe3a69e3142454f6fd8440116d6b4e164b6e53
joana.casallas <joacasallas@gmail.com>
2026-04-27 19:24:09 -0500

    chore: setup eslint base configuration , chore: add prettier configuration, chore: add docker setup with postgres and redis, chore: setup github actions CI pipeline,

---
+++ packages/config/eslint.base.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+};
